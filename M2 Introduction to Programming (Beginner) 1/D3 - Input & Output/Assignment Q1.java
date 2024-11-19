d0d5a685138a718ddb6072993812c289fc2452f3
Priyanshu279 <279priii@gmail.com>
2024-11-19 22:48:47 +0530

    Initial folder strucutre

---
+++ M2 Introduction to Programming (Beginner) 1/D3 - Input & Output/Assignment Q1.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name1=sc.next();
+        String name2=sc.next();
+        System.out.println(name1+" says Hi to "+name2);
+    }
+}
---
+++ M2 Introduction to Programming (Beginner) 1/D3 - Input & Output/Assignment Q2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+	public static void main(String args[]) {
+		// Your code goes here
+		String s1="A";
+		String s2="B";
+		String s3="C";
+		String s4="D";
+		String s5="E";
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(s4);
+		System.out.println(s5);
+	}
+}
