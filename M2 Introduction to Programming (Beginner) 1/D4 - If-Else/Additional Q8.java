1ae6db0a846af27caa09ee850b90f72409ebc07c
Priyanshu279 <279priii@gmail.com>
2024-11-21 17:50:52 +0530

    Additional problem 8 optmized

--- M2 Introduction to Programming (Beginner) 1/D4 - If-Else/Additional Q8.java
+++ M2 Introduction to Programming (Beginner) 1/D4 - If-Else/Additional Q8.java
@@ -4,45 +4,15 @@ class Main {
 	public static void main(String args[]) {
 		// Your code goes here
 		Scanner sc=new Scanner(System.in);
-		int A=sc.nextInt();
-		if(A==1){
-			System.out.println(31);
-		}
-		else if(A==2){
-			System.out.println(28);
-		}
-		else if(A==3){
-			System.out.println(31);
-		}
-		else if(A==4){
-			System.out.println(30);
-		}
-		else if(A==5){
-			System.out.println(31);
-		}
-		else if(A==6){
-			System.out.println(30);
-		}
-		else if(A==7){
-			System.out.println(31);
-		}
-		else if(A==8){
-			System.out.println(31);
-		}
-		else if(A==9){
-			System.out.println(30);
-		}
-		else if(A==10){
-			System.out.println(31);
-		}
-		else if(A==11){
-			System.out.println(30);
-		}
-		else if(A==12){
-			System.out.println(31);
-		}
-		else{
-			System.out.println("Invalid Month Number");
-		}
+		  int A = sc.nextInt();
+        if(A == 2){
+            System.out.println("28");
+        }
+        else if( A == 1 || A == 3 || A == 5 || A == 7 || A == 8 || A == 10 || A == 12 ){
+            System.out.println("31");
+        }
+        else{
+            System.out.println("30");
+        }
 	}
 }
