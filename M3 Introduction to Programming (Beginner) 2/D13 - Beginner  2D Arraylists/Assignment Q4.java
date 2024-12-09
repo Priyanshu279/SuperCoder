3772d2604c5816ea31f7595cee7df86fbdc37a30
Priyanshu279 <279priii@gmail.com>
2024-12-10 00:00:44 +0530

    2DArraylist Assignment question

--- M3 Introduction to Programming (Beginner) 2/D13 - Beginner  2D Arraylists/Assignment Q4.java
+++ M3 Introduction to Programming (Beginner) 2/D13 - Beginner  2D Arraylists/Assignment Q4.java
@@ -25,41 +25,14 @@ class Main {
 	public static void main(String args[]) {
 		// Your code goes here
 			Scanner sc=new Scanner(System.in);
-			// creation of 2d arraylist
-			ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
 			
-			//No of arraylist inside list(2d array)
 			int n=sc.nextInt();
-		
-			for(int i=0;i<n;i++){
-				//Add logic to take an arraylist as input
-				int size=sc.nextInt();
-				ArrayList<Integer> l=new ArrayList<Integer>();
-				for(int j=0;j<size;j++){
-					l.add(sc.nextInt());
-				}
-				//and add it to 2D arraylist
-				list.add(l);
-			}
-			
-			//printing 2d arraylist
-			for(int i=0;i<list.size();i++){
-				// Print ith list
-				for(int j=0;j<list.get(i).size();j++){
-					System.out.print(list.get(i).get(j)+" ");
-				}
-				System.out.println();
-			}
-			
-			//printing 2d array by another way
-			System.out.println(list);
-			
+
 			// for staircase pattern printing
 			ArrayList<ArrayList<Integer>> pattern = staircasePattern(n);
 			
 			//printing staircase pattern
 			for(int i=0;i<pattern.size();i++){
-				// Print ith list
 				for(int j=0;j<pattern.get(i).size();j++){
 					System.out.print(pattern.get(i).get(j)+" ");
 				}
