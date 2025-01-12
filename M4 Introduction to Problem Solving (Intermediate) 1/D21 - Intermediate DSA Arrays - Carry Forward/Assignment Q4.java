2631842990dde3b589185f7a54f6a3e5d8a4e68e
priyanshu279 <279priii@gmail.com>
2025-01-12 16:00:11 +0530

    Optmized

--- M4 Introduction to Problem Solving (Intermediate) 1/D21 - Intermediate DSA Arrays - Carry Forward/Assignment Q4.java
+++ M4 Introduction to Problem Solving (Intermediate) 1/D21 - Intermediate DSA Arrays - Carry Forward/Assignment Q4.java
@@ -1,48 +1,53 @@
 import java.util.*;
 
 class Main {
-    public static int smallestSubarrayLengthWithMinAndMax(int[] A) {
-        int N = A.length;
-        int minVal = Integer.MAX_VALUE;
-        int maxVal = Integer.MIN_VALUE;
-
-        int minIndex=-1;
+    
+    public static int lenghtOfClosestMinMax(int A[]){
+        int N=A.length;
         int maxIndex=-1;
-		
-        for (int i = 0; i < N; i++) {
-            minVal = Math.min(minVal, A[i]);
-            maxVal = Math.max(maxVal, A[i]);
+        int minIndex=-1;
+        int ansLength=N+1;
+        int maxVal=A[0];
+        int minVal=A[0];
+        
+        for(int i=0;i<N;i++){
+            if(A[i]>maxVal){
+                maxVal=A[i];
+            }
+            else if(A[i]<minVal){
+                minVal=A[i];
+            }
         }
-
-        int ansLength = Integer.MAX_VALUE;
-
+        
         for(int i=N-1;i>=0;i--){
             if(A[i]==minVal){
                 minIndex=i;
                 if(maxIndex!=-1){
-                    ansLength=Math.min(ansLength,maxIndex-minIndex+1);
+                    ansLength=Math.min(ansLength,(maxIndex-minIndex)+1);
                 }
             }
             else if(A[i]==maxVal){
                 maxIndex=i;
                 if(minIndex!=-1){
-                    ansLength=Math.min(ansLength,minIndex-maxIndex+1);
+                    ansLength=Math.min(ansLength,(minIndex-maxIndex)+1);
                 }
             }
         }
         return ansLength;
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-		
-        int N = sc.nextInt();
-        int[] A = new int[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+    
+	public static void main(String args[]) {
+		// Your code goes here
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int A[]=new int[N];
+        
+        for(int i=0;i<N;i++){
+            A[i]=sc.nextInt();
         }
-
-        int result = smallestSubarrayLengthWithMinAndMax(A);
-        System.out.println(result);
-    }
+        
+        int length=lenghtOfClosestMinMax(A);
+        System.out.println(length);
+        
+	}
 }
