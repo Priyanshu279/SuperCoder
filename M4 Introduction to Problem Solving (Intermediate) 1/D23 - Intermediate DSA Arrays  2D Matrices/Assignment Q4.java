6fab4de9bfe8d1bd4335fda41b4f4b643d8c5a22
priyanshu279 <279priii@gmail.com>
2025-01-17 22:21:54 +0530

    2DM

---
+++ M4 Introduction to Problem Solving (Intermediate) 1/D23 - Intermediate DSA Arrays  2D Matrices/Assignment Q4.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Main {
+    
+    public static int minorDiagonalSum(int A[][]){
+        int N=A.length;
+        int sum=0;
+        int i=0;
+        int j=N-1;
+        
+        while(j>=0){
+            sum=sum+A[i][j];
+            i++;
+            j--;
+        }
+        return sum;
+    }
+    
+	public static void main(String args[]) {
+		// Your code goes here
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int A[][]=new int[N][N];
+        
+        for(int row=0;row<N;row++){
+            for(int col=0;col<N;col++){
+                A[row][col]=sc.nextInt();
+            }
+        }
+        
+        int ans=minorDiagonalSum(A);
+        System.out.println(ans);
+	}
+}
